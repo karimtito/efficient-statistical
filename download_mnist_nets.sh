3903fc746ed5a90d2e6e62c12ce2174bdf9e7f26
karimtito <karim.tit@ensae-paristech.fr>
2021-06-05 09:09:59 +0200

    adding script to download MNIST networks

---
+++ download_mnist_nets.sh
@@ -0,0 +1,6 @@
+cd ./data/MNIST/test_nets/
+wget  https://files.sri.inf.ethz.ch/eran/nets/pytorch/mnist/convMedGRELU__PGDK_w_0.1.pyt
+wget  https://files.sri.inf.ethz.ch/eran/nets/pytorch/mnist/ffnnRELU__PGDK_w_0.1_6_500.pyt
+wget  https://files.sri.inf.ethz.ch/eran/nets/pytorch/mnist/ffnnRELU__Point_6_500.pyt
+wget  https://files.sri.inf.ethz.ch/eran/nets/tensorflow/mnist/mnist_relu_9_200.tf
+cd ../../../
